ec80d4e8e706a7f9c1a8eff4782338bc68c506cb
hotwords123 <hotwords123@gmail.com>
2024-05-04 23:31:32 +0800

    feat: add third person camera controller

--- include/control.h
+++ include/control.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
 class Camera;
 
 class FirstPersonController {
@@ -24,4 +26,41 @@ private:
 
     float movement_speed_;
     float mouse_sensitivity_;
+};
+
+class ThirdPersonController {
+public:
+    ThirdPersonController(Camera& camera, float rotation_speed, float zoom_base);
+
+    void set_view(float fov, float height);
+
+    glm::vec3 target() const { return target_; }
+    void set_target(glm::vec3 target) { target_ = target; }
+
+    float distance() const { return distance_; }
+    void set_distance(float distance) { distance_ = distance; }
+
+    float yaw() const { return yaw_; }
+    void set_yaw(float yaw) { yaw_ = yaw; }
+
+    float pitch() const { return pitch_; }
+    void set_pitch(float pitch) { pitch_ = pitch; }
+
+    void rotate(float x_offset, float y_offset);
+    void translate(float x_offset, float y_offset);
+    void zoom(float offset);
+
+    void update_camera() const;
+
+private:
+    Camera& camera_;
+
+    float movement_speed_;
+    float rotation_speed_;
+    float zoom_base_;
+
+    glm::vec3 target_;
+    float distance_;
+    float yaw_;
+    float pitch_;
 };
--- src/control.cpp
+++ src/control.cpp
@@ -52,3 +52,54 @@ void FirstPersonController::handle_mouse(float x_offset, float y_offset) {
     camera_.set_yaw(yaw);
     camera_.update();
 }
+
+ThirdPersonController::ThirdPersonController(Camera& camera, float rotation_speed, float zoom_base)
+    : camera_(camera),
+      movement_speed_(),
+      rotation_speed_(rotation_speed),
+      zoom_base_(zoom_base),
+      target_(),
+      distance_(),
+      yaw_(),
+      pitch_() {
+}
+
+void ThirdPersonController::set_view(float fov, float height) {
+    movement_speed_ = 2 * tan(glm::radians(fov) / 2) / height;
+}
+
+void ThirdPersonController::rotate(float x_offset, float y_offset) {
+    pitch_ -= y_offset * rotation_speed_;
+    yaw_ += x_offset * rotation_speed_;
+
+    if (pitch_ > 89.0f) {
+        pitch_ = 89.0f;
+    } else if (pitch_ < -89.0f) {
+        pitch_ = -89.0f;
+    }
+
+    yaw_ = fmod(yaw_, 360.0f);
+
+    update_camera();
+}
+
+void ThirdPersonController::translate(float x_offset, float y_offset) {
+    glm::vec3 offset = x_offset * camera_.right() - y_offset * camera_.up();
+    target_ -= movement_speed_ * distance_ * offset;
+    update_camera();
+}
+
+void ThirdPersonController::zoom(float offset) {
+    distance_ *= pow(zoom_base_, offset);
+    update_camera();
+}
+
+void ThirdPersonController::update_camera() const {
+    glm::vec3 front(
+        cos(glm::radians(yaw_)) * cos(glm::radians(pitch_)),
+        sin(glm::radians(pitch_)),
+        sin(glm::radians(yaw_)) * cos(glm::radians(pitch_))
+    );
+    camera_.set_position(target_ - distance_ * front);
+    camera_.look_at(target_);
+}
